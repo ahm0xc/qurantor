50922287362884c5f17b911042bd89b84957e79d
ahm0xc <codingwebsa@gmail.com>
2024-06-05 14:42:37 +0600

    add home page and move docs to /docs

---
+++ apps/docs/src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Link from "next/link";
+
+export default function HomePage() {
+  return (
+    <div>
+      HomePage
+      <Link href="/docs">Docs</Link>
+    </div>
+  );
+}
