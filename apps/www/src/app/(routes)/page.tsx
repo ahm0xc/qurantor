b048506d2ea9cad5a2a0664e5c100e23dd12984b
ahm0xc <codingwebsa@gmail.com>
2024-06-17 23:15:06 +0600

    basic surah route setup

--- apps/www/src/app/(routes)/page.tsx
+++ apps/www/src/app/(routes)/page.tsx
@@ -1,15 +1,5 @@
-import { getInfo } from "~/helpers/info";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const info = getInfo();
-
-  return (
-    <main>
-      <div>
-        {info.chapters.map((chapter, idx) => {
-          return <div key={`home-chapter-${idx}`}>{chapter.englishname}</div>;
-        })}
-      </div>
-    </main>
-  );
+  redirect("/1");
 }
