f97d4b4b33475dba7eb97cf73accb2a89d026415
ahm0xc <codingwebsa@gmail.com>
2024-06-27 22:11:04 +0600

    test for qurantor sqllite db

--- apps/www/next.config.mjs
+++ apps/www/next.config.mjs
@@ -1,3 +1,5 @@
+await import("./src/env.mjs");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
---
+++ apps/www/src/app/(routes)/test/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { db } from '~/server/db'
+import { posts } from '~/server/db/schema'
+
+export default async function TestPage() {
+    await db.insert(posts).values({
+        name: 'Test post',
+    })
+    const postsData = await db.select().from(posts)
+
+  return (
+    <div>
+        {postsData.map(post => (
+            <div key={`hello-${post.id}`}>{post.id} - {post.name}</div>
+        ))}
+    </div>
+  )
+}
