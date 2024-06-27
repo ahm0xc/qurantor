e7a8fc92cb5a38e9a5dd7b53410e026b8d57816d
ahm0xc <codingwebsa@gmail.com>
2024-06-27 22:19:44 +0600

    add optional flag

--- apps/www/src/env.mjs
+++ apps/www/src/env.mjs
@@ -10,7 +10,8 @@ export const env = createEnv({
     DATABASE_URL: z.string().url(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
-      .default("development"),
+      .default("development")
+      .optional(),
   },
 
   /**
